cb2e697da2ec8dca1943b0f500c86ae0bd1f768d
Sourav <sourav.khilari.it26@heritageit.edu.in>
2024-12-19 12:49:28 +0530

    add db

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: "./.env",
+})
+
+connectDB()
+.then(()=>{
+    app.on("error",(error )=>{
+        console.log("ERROR:",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running on port ${process.env.port}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo Db connection failed!!!",err);
+    
+})
