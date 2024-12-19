95c62c9d98db1072437f598388d6c1af56eacdcd
Sourav <sourav.khilari.it26@heritageit.edu.in>
2024-12-19 13:01:39 +0530

    `Removed watchHistory field and added password field in user model schema`

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -33,12 +33,7 @@ const userSchema=new Schema({
     coverImage:{
         type:String,//cloudinary url
     },
-    watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref:"Video"
-        }
-    ],
+    
     password:{
         type:String,
         required:[true,'Password is required'],
