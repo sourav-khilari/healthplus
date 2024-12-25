70a00b6291ab1454c7e5c4d972143e016437b250
suhridp <suhridbpaul@gmail.com>
2024-12-25 18:38:25 +0530

    added frontend element

--- backend/src/routes/hospital.route.js
+++ backend/src/routes/hospital.route.js
@@ -1,17 +1,31 @@
 import { Router } from "express";
-import { registerHospital,loginHospital,addDoctor,getDoctorAppointments,getHospitalAllAppointments} from '../controllers/hospital.controller.js'
-import {authMiddleware,roleMiddleware} from '../middlewares/auth.middleaware.js'
+import {
+  registerHospital,
+  loginHospital,
+  addDoctor,
+  getDoctorAppointments,
+  getHospitalAllAppointments,
+} from "../controllers/hospital.controller.js";
+import {
+  authMiddleware,
+  roleMiddleware,
+} from "../middlewares/auth.middleaware.js";
 
+const router = Router();
+router.post("/register", registerHospital);
+router.post("/login", loginHospital);
 
+router.post("/addDoctor", roleMiddleware("hospital"), addDoctor);
 
-const router = Router()
+router.get(
+  "/getDoctorAppointments",
+  roleMiddleware("hospital"),
+  getDoctorAppointments,
+);
+router.get(
+  "/getHospitalAllAppointments",
+  roleMiddleware("hospital"),
+  getHospitalAllAppointments,
+);
 
-router.post('/register', registerHospital);
-router.post('/login', loginHospital);
-
-router.post('/addDoctor',roleMiddleware("hospital"), addDoctor);
-
-router.get('/getDoctorAppointments',roleMiddleware("hospital"), getDoctorAppointments);
-router.get('/getHospitalAllAppointments',roleMiddleware("hospital"), getHospitalAllAppointments);
-
-export default router
+export default router;
