c9e6af06e0dab9b10b4c592c8db3154934698ec7
suhridp <suhridbpaul@gmail.com>
2024-12-26 01:58:43 +0530

    added some dependencies

--- backend/src/routes/sub.admin.route.js
+++ backend/src/routes/sub.admin.route.js
@@ -32,11 +32,11 @@ import {
   markOrderAsDelivered,
 } from "../controllers/medicalstore.controller/orderController.js";
 
-import { upload } from "../middlewares/multer.middleware.js"
+import { upload } from "../middlewares/multer.middleware.js";
 
 import { roleMiddleware } from "../middlewares/auth.middleaware.js";
 
-const router = Router()
+const router = Router();
 
 router.post("createCategory", roleMiddleware("admin"), createCategory);
 router.put("/:categoryId", roleMiddleware("admin"), updateCategory);
@@ -45,37 +45,50 @@ router.delete("/:categoryId", roleMiddleware("admin"), removeCategory);
 router.get("/categories", listCategory);
 router.get("/:id", readCategory);
 
-
-
 route.get("getAllOrders", roleMiddleware("admin"), getAllOrders);
 
 router.get("/total-sales", roleMiddleware("admin"), calculateTotalSales);
-router.get("/total-sales-by-date", roleMiddleware("admin"), calcualteTotalSalesByDate);
+router.get(
+  "/total-sales-by-date",
+  roleMiddleware("admin"),
+  calcualteTotalSalesByDate,
+);
 
 router.put("/:id/deliver", roleMiddleware("admin"), markOrderAsDelivered);
 
-
-router.get("/", fetchProducts)
+router.get("/", fetchProducts);
 //router.post("/addProduct", roleMiddleware("admin"),, addProduct);//
 
-router.post("/addProduct", roleMiddleware("admin"), upload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }, { name: 'image4', maxCount: 1 }]), addProduct);
-
-
+router.post(
+  "/addProduct",
+  roleMiddleware("admin"),
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct,
+);
 
 router.get("/allproducts", fetchAllProducts);
-
-
 router.get("/top", fetchTopProducts);
 router.get("/new", fetchNewProducts);
 
-
-
-router.get("/:id", fetchProductById)
-router.put("/:id", roleMiddleware("admin"), upload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }, { name: 'image4', maxCount: 1 }])
-  , updateProductDetails)//
+router.get("/:id", fetchProductById);
+router.put(
+  "/:id",
+  roleMiddleware("admin"),
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  updateProductDetails,
+); //
 router.delete("/:id", roleMiddleware("admin"), removeProduct);
 
 router.route("/filtered-products").post(filterProducts);
-
 
 export default router;
--- frontend/src/pages/MedicalStore/Redux/constants.js
+++ frontend/src/pages/MedicalStore/Redux/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
+export const BASE_URL = "http://localhost:5173";
+export const USERS_URL = "/api/v1/users";
+export const CATEGORY_URL = "/api/v1/category";
+export const PRODUCT_URL = "/api/v1/products";
+export const UPLOAD_URL = "/api/v1/upload";
+export const ORDERS_URL = "/api/v1/orders";
+export const PAYPAL_URL = "/api/v1/config/paypal";
