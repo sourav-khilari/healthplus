4f7685dfa858d4e45e75cc593877b62625db21ad
suhridp <suhridbpaul@gmail.com>
2024-12-26 19:41:16 +0530

    added users from user

--- frontend/src/pages/MedicalStore/Redux/constants.js
+++ frontend/src/pages/MedicalStore/Redux/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "http://localhost:8000";
-export const USERS_URL = "/api/v1/user";
+export const USERS_URL = "/api/v1/users";
 export const ADMIN_URL = "/api/v1/admin";
 export const PAYPAL_URL = "/api/v1/config/paypal";
