f7b933748df55b745ce10e8cd5ad8c61d7d90c82
Sourav <sourav.khilari.it26@heritageit.edu.in>
2025-01-30 22:40:25 +0530

    `Fixed commented out server listen code`

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,9 +13,9 @@ connectDB()
       console.log("ERROR:", error);
       throw error;
     });
-    // server.listen(process.env.PORT || 8000, () => {
-    //   console.log(`Server is running on port ${process.env.PORT}`);
-    // });
+    server.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
   })
   .catch((err) => {
     console.log("Mongo Db connection failed!!!", err);
